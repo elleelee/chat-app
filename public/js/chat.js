abbd4489f34b4da1223afe6a39a12f6dcfa2f09f
Ellen Lee <ellen811003@gmail.com>
2019-10-02 23:03:45 +0800

    rendered messages and location in html

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,10 +5,26 @@ const $messageForm = document.querySelector('#message-form')
 const $messageFormInput = document.querySelector('input')
 const $messageFormButton = document.querySelector('button')
 const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
 
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
 socket.on('message', (message) => {
   console.log(message)
+  const html = Mustache.render(messageTemplate, {
+    message
+  })
+  $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (url) => {
+  console.log(url)
+  const html =Mustache.render(locationMessageTemplate, {
+    url
+  })
+  $messages.insertAdjacentHTML('beforeend', html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
