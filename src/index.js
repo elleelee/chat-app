a33cfc1151b13eca5eeb24b6a390010ef5d6aaf6
Ellen Lee <ellen811003@gmail.com>
2019-10-02 13:29:13 +0800

    added broadcast login/out messages & geolocation sharing

--- src/index.js
+++ src/index.js
@@ -12,17 +12,24 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0
 
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
-    socket.emit('countUpdated', count)
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on('increment', () => {
-        count++
-        // socket.emit('countUpdated', count)
-        io.emit('countUpdated', count)
+    socket.on('sendMessage', (message, callback) => {
+      io.emit('message', message)
+      callback('Delivered')
+    })
+
+    socket.on('disconnect', () => {
+      io.emit('message', 'A user has left!')
+    })
+
+    socket.on('sendLocation', (coords) => {
+      io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
     })
 })
 
