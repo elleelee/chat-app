70c6fa6f7524621c5f5a3ff6bd415312419f662f
Ellen Lee <ellen811003@gmail.com>
2019-10-03 10:04:50 +0800

    added time for messages and location messages'

--- public/js/chat.js
+++ public/js/chat.js
@@ -14,17 +14,19 @@ const locationMessageTemplate = document.querySelector('#location-message-templa
 socket.on('message', (message) => {
   console.log(message)
   const html = Mustache.render(messageTemplate, {
-    message
+    message: message.text,
+    createdAt: moment(message.createdAt).format('h:mm a')
   })
   $messages.insertAdjacentHTML('beforeend', html)
 })
 
-socket.on('locationMessage', (url) => {
-  console.log(url)
-  const html =Mustache.render(locationMessageTemplate, {
-    url
-  })
-  $messages.insertAdjacentHTML('beforeend', html)
+socket.on('locationMessage', (message) => {
+    console.log(message)
+    const html = Mustache.render(locationMessageTemplate, {
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
 })
 
 $messageForm.addEventListener('submit', (e) => {
