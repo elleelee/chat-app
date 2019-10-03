f841a70f920e01a4a8eb6192b640defe872cd3e6
Ellen Lee <ellen811003@gmail.com>
2019-10-03 11:01:35 +0800

    added functions to add/remove/getUser/getUsersInRoom

---
+++ src/utils/users.js
@@ -0,0 +1,54 @@
+const users = []
+
+// addUser, removeUser, getUser, getUsersInRoom
+const addUser = ({ id, username, room }) => {
+  username = username.trim().toLowerCase()
+  room = room.trim().toLowerCase()
+
+  if (!username || !room) {
+    return {
+      error: 'Username & room are required!'
+    }
+  }
+
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username
+  })
+
+  if(existingUser) {
+    return {
+      error: 'Username is in use!'
+    }
+  }
+
+  const user = { id, username, room }
+  users.push(user)
+  return { user }
+}
+
+
+const removeUser =(id) => {
+  const index = users.findIndex((user) => id === user.id)
+
+  if (index !== -1) {
+    return users.splice(index, 1)[0]
+  }
+}
+
+const getUser =(id) => {
+  return users.find((user) => id === user.id)
+}
+
+const getUsersInRoom =(room) => {
+  room = room.trim().toLowerCase()
+  return users.filter((user) => room === user.room)
+}
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
+}
+
+
